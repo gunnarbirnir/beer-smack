1d4ee72ffb5f9489246358bcc8b1752620f0dd21
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2021-12-13 22:27:24 +0100

    Reset room confirm prompt

--- scripts/resetRoom.js
+++ scripts/resetRoom.js
@@ -1,6 +1,20 @@
+const readline = require('readline');
+
 const roomActions = require('./roomActions');
 
-roomActions.resetRoom(process.argv[2]).then((room) => {
-  console.log(`Room ${room.title} has been reset`);
-  process.exit();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Are you sure? (y/n): ', function (confirm) {
+  if (confirm === 'y') {
+    roomActions.resetRoom(process.argv[2]).then((room) => {
+      console.log(`Room ${room.title} has been reset`);
+      process.exit();
+    });
+  } else {
+    console.log('Reset canceled');
+    process.exit();
+  }
 });
